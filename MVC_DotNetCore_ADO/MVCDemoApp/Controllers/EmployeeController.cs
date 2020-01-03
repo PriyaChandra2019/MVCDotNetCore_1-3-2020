ed0def4b21ffa880629d42bb0db3b5f1727ee77e
Priya Chandra <ChandruPriya@gmail.com>
2020-01-03 09:49:02 -0800

    MVC demo App implemented using dot Net core and ADO. Used SQL server tables & stored procedures for CRUD operations.

---
+++ MVC_DotNetCore_ADO/MVCDemoApp/Controllers/EmployeeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCDemoApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCDemoApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        EmployeeDataAccessLayer objemployee = new EmployeeDataAccessLayer();
+
+        // Adding the logic of create View
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                objemployee.AddEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Employee> lstEmployee = new List<Employee>();
+            lstEmployee = objemployee.GetAllEmployees().ToList();
+
+            return View(lstEmployee);
+        }
+
+        //Adding the logic of Edit View
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Employee employee = objemployee.GetEmployeeData(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind]Employee employee)
+        {
+            if (id != employee.ID)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                objemployee.UpdateEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        //Adding Logic for Details View
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Employee employee = objemployee.GetEmployeeData(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        //Adding Logic to Delete View
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Employee employee = objemployee.GetEmployeeData(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int? id)
+        {
+            objemployee.DeleteEmployee(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
